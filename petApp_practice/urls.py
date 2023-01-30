aa01ee9fac626b8ea316916817dfb2a58c72ccfd
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2023-01-30 19:28:56 +0300

    work in progress

--- petApp_practice/urls.py
+++ petApp_practice/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from .import views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('details/<int:id>/', views.pet_details, name="pet_details")
 ]
-urlpatterns += staticfiles_urlpatterns()
